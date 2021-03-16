df9d099ee123715739f3c05b217a0af02c699e03
Shipix <shipix@hotmail.fr>
2021-03-16 10:58:41 +0100

    Partie 1

---
+++ resources/views/store/FormAddPkmn.blade.php
@@ -0,0 +1,21 @@
+<form class="container mt-5" action="" method="">
+    <div class="form-group">
+      <label for="nom">Nom</label>
+      <input type="texte" class="form-control" id="nom" name="nom">
+    </div>
+    <div class="form-group">
+      <label for="type">Type</label>
+      <select class="form-control" id="type" name="type">
+        <option>1</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="src" class="form-label">Photo</label>
+      <input type="file" class="form-control" id="src" name="src">
+  </div>
+  <div class="form-group">
+    <label for="Level">level</label>
+    <input type="number" class="form-control" id="level" name="level">
+  </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
---
+++ resources/views/store/FormAddType.blade.php
@@ -0,0 +1,6 @@
+<form class="container mt-5" action="" method="">
+    <div class="form-group">
+        <label for="nom">Nom</label>
+        <input type="texte" class="form-control" id="nom" name="nom">
+    </div>
+</form>
